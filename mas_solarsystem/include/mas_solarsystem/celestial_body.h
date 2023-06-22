e64b3c961165c63e13e99c1526be24f68fdf5fdb
khaled <khaled.yahyaoui@esprit.tn>
2023-06-22 12:42:11 +0200

    on a ajouter des dossier

---
+++ mas_solarsystem/include/mas_solarsystem/celestial_body.h
@@ -0,0 +1,13 @@
+#ifndef __MAS_SOLARSYSTEM__CELESTIAL_BODY_H__
+#define __MAS_SOLARSYSTEM__CELESTIAL_BODY_H__
+#include <rclcpp/rclcpp.hpp>
+//...
+class CelestialBody : public rclcpp::Node
+{
+public:
+CelestialBody();
+private:
+//... 
+
+};
+#endif // __MAS_SOLARSYSTEM__CELESTIAL_BODY_H__
